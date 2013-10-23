2541b3f8b838974caf0b056f0c18bc7d5471322b
Oskar Truffer <oskar.truffer@gmail.com>
2013-10-23 15:57:21 +0200

    comment

--- app/controllers/devise_iuvenes_controller.rb
+++ app/controllers/devise_iuvenes_controller.rb
@@ -1,3 +1,4 @@
 class DeviseIuvenesController < Devise::SessionsController
+  #we dont want protect from forgery for login, so the app can log in.
     protect_from_forgery :except => [:new, :create]
 end
